66087757143996aab8e53d716c4c196250024b71
Will James <jameswt@gmail.com>
2014-11-19 12:32:11 -0600

    starting to set up downstream-node python app

---
+++ roles/dsnode/templates/config.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import heartbeat
+
+# Flask
+SECRET_KEY = os.urandom(32)
+
+# SQLAlchemy (DB)
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://downstream:{{ lookup('password','credentials/mysqlpassword') }}@localhost/downstream'  # NOQA
+
+FILES_PATH = '/tmp/'
+TAGS_PATH = '/tmp/'
+MMDB_PATH = '{{ dsnodegeodbpath }}'
+
+HEARTBEAT = heartbeat.Swizzle.Swizzle
+
+TEST_FILE_SIZE = 100
+MAX_TOKENS_PER_IP = 5
+MIN_SJCX_BALANCE = 10000
+MAX_SIG_MESSAGE_SIZE = 1024
+REQUIRE_SIGNATURE = False
+

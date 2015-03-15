755d25961bf9a63e5507f18a7e7e0beedad2f1b8
Will James <jameswt@gmail.com>
2015-03-15 16:35:40 +1100

    added tag key for remote heartbeat request

--- roles/dsnode/templates/config.py
+++ roles/dsnode/templates/config.py
@@ -13,13 +13,19 @@ SERVER_ALIAS = '{{ dsnodehostname }}'
 # SQLAlchemy (DB)
 SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://{{ dsnodemysqluser }}:{{ mysqlpassword }}@{{ mysqldbserver }}/{{dsnodemysqldbname}}?charset=utf8'  # NOQA
 
+TAG_KEY = '{{dsnodetagskey}}'
+
 FILES_PATH = '{{ dsnodetmppath }}'
 TAGS_PATH = '{{ dsnodetagspath }}'
 REMOTE_TAGS_PATH = '{{ dsnoderemotetagspath }}'
 MMDB_PATH = '{{ dsnodegeodbpath }}'
 
 HEARTBEAT = heartbeat.Merkle.Merkle
+{% if (inventory_hostname == groups['dbserver'][0]) %}
 HEARTBEAT_PATH = '{{ dsnodeheartbeatpath }}'
+{% else %}
+HEARTBEAT_PATH = '{{ dsnoderemoteheartbeatpath }}'
+{% endif %}
 
 MONGO_LOGGING = True
 MONGO_URI = 'mongodb://{{dsnodemongodbuser}}:{{mongodbpassword}}@{{mongodbserver}}/{{dsnodemongodbname}}'
